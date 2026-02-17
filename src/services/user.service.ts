5d756edf26e908cfba941b45489af3aba8cb40fe
Kelvin Orhungul <kelvin.orhungul@gmail.com>
2026-02-17 12:09:17 +0100

    fix build issue

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -14,6 +14,7 @@ class UserService {
         id: true,
         fullName: true,
         role: true,
+        gender: true,
         profileImage: true,
         brandName: true,
         brandLogo: true,
